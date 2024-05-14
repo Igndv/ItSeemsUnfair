cf353c656b4a6f26bc55637ea734f0834dc9029d
Igndv <ignasiusdv@gmail.com>
2024-05-14 21:06:52 +0700

    feat : space to roll dice fixed

--- ItSeemsUnfair/ItSeemsUnfair.cpp
+++ ItSeemsUnfair/ItSeemsUnfair.cpp
@@ -3,40 +3,74 @@
 
 #include <iostream>
 #include "raylib.h"
+#include <time.h>
+#include <conio.h>
 
 using namespace std;
+
+//assign class
+class stats {
+protected:
+	int HP, damage;
+
+public:
+	
+};
+
+class animation {
+
+};
+
+class interaction {
+
+};
+
+//func sementara
 
 void diceBattle() { //battle dice tapi belum dgn kbhit space, +interaction when win dice
 	int dice1, dice2;
 	bool roundWinner;
+	char ch;
+	srand(time(0));
 
 	cout << "Press space to roll dice!" << endl;
-	dice1 = rand() % 7;
-	dice2 = rand() % 7;
+	ch = _getch();
+	if (ch==' ') {
+		dice1 = (rand() % 6) + 1;
+		dice2 = (rand() % 6) + 1;
 
-	if (dice1 > dice2) {
-		cout << "Your Chance to attack!" << endl;
-		roundWinner = 1;
+		if (dice1 > dice2) {
+			cout << "Your Roll : " << dice1 << " | Your Enemy Roll : " << dice2 << endl << "Your Chance to attack!" << endl;
+			roundWinner = 1;
 
+		}
+		else if (dice1 < dice2) {
+			cout << "Your Roll : " << dice1 << " | Your Enemy Roll : " << dice2 << endl << "Enemy's Chance to attack!" << endl;
+			roundWinner = 0;
+
+		}
+		else if (dice1 == dice2) {
+			cout << "TIE" << endl;
+			diceBattle();
+		}
 	}
-	else if (dice1 < dice2) {
-		cout << "Enemy's Chance to attack!" << endl;
-		roundWinner = 0;
-
-	}
-	else if (dice1 == dice2) {
+	else {
 		diceBattle();
 	}
 }
 
+void interaction() {
+
+}
+
 void battle() {
 	int playerCount, enemyCount, playerIndex, enemyIndex;
-	bool turn_check = 0, player_check = 0, enemy_check = 0;
+	bool playerIsAlive = 1, enemyIsAlive = 1; //checks if there is still player or enemies are still alive
 
 	playerCount = 1;//player and enemy count masih misal untuk test aja
 	enemyCount = 2;
 
-	while (player_check != 0 || enemy_check != 0) {	//selama check player / enemy belum mati jalan tros.. +dice battle interaction belum
+	while (playerIsAlive == 1 || enemyIsAlive == 1) {	//selama check player / enemy belum mati jalan tros.. +dice battle interaction belum
 		for (playerIndex = 1; playerIndex <= playerCount; playerIndex++) {
 
 			for (enemyIndex = 1; enemyIndex <= enemyCount; enemyIndex++) {
@@ -49,7 +83,8 @@ void battle() {
 
 int main()
 { 
-	cout << "Hello World";
+	cout << "Hello World"<<endl;
+	battle();
 		return 0;
 }
 
